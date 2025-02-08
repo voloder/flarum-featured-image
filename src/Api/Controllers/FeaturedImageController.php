63cdd9f8a539ea8f49fb06ec5ef55331d7a43ce3
voloder <110066198+voloder@users.noreply.github.com>
2025-02-08 21:13:35 +0100

    require fof/upload

---
+++ src/Api/Controllers/FeaturedImageController.php
@@ -0,0 +1,10 @@
+<?php
+namespace Voloder\FlarumFeaturedImage\Api\Controllers;
+
+
+use FoF\Upload\Api\Controllers\UploadController;
+
+
+class FeaturedImageController extends UploadController {
+
+}
