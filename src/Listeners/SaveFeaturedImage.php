c15fa2b14795bb3040fc4cd6b78e585f4f759b05
voloder <110066198+voloder@users.noreply.github.com>
2025-02-08 23:39:22 +0100

    log error

--- src/Listeners/SaveFeaturedImage.php
+++ src/Listeners/SaveFeaturedImage.php
@@ -3,17 +3,25 @@
 namespace Voloder\FlarumFeaturedImage\Listeners;
 
 use Flarum\User\Event\Saving;
+use Illuminate\Support\Facades\Log;
 
 class SaveFeaturedImage
 {
 
     public function handle(Saving $event)
     {
-        $user = $event->user;
-        $data = $event->data;
+        try {
+            $user = $event->user;
+            $data = $event->data;
 
-        if (isset($data["attributes"]["featuredImage"])) {
-            $user->featuredImage = $data["attributes"]["featuredImage"];
+            if (isset($data["attributes"]["featuredImage"])) {
+                $user->featuredImage = $data["attributes"]["featuredImage"];
+            }
+        } catch (\Exception $e) {
+            Log::error('Error in SaveFeaturedImage: ' . $e->getMessage());
+
+            throw $e;
         }
+
     }
 }
